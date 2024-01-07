113f23b9edacfd4211e7f88818c6e2f57389157e
Mehmet Gürsoy <mehmet.gursoy@windowslive.com>
2024-01-07 21:14:40 +0300

    Web projesi eklendi. IdentityService içerisindeki SignIn metodu ve cookie işlemleri tamamlandı.

---
+++ Frontends/FreeCourse.Web/Models/SignInInput.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FreeCourse.Web.Models
+{
+    public class SignInInput
+    {
+        [Display(Name = "E-Posta Adresi")]
+        public string Email { get; set; }
+
+        [Display(Name = "Şifre")]
+        public string Password { get; set; }
+
+        [Display(Name = "Beni Hatırla")]
+        public bool IsRemember { get; set; }
+    }
+}
---
+++ Frontends/FreeCourse.Web/Services/IdentityService.cs
@@ -0,0 +1,118 @@
+﻿using FreeCourse.Shared.Dtos;
+using FreeCourse.Web.Models;
+using FreeCourse.Web.Services.Interfaces;
+using IdentityModel.Client;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using System.Globalization;
+using System.Security.Claims;
+using System.Text.Json;
+
+namespace FreeCourse.Web.Services
+{
+    public class IdentityService : IIdentityService
+    {
+        private readonly HttpClient _httpClient;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        private readonly ClientSettings _clientSettings;
+        private readonly ServiceApiSettings _serviceApiSettings;
+
+        public IdentityService(HttpClient httpClient, IHttpContextAccessor httpContextAccessor, IOptions<ClientSettings> clientSettings, IOptions<ServiceApiSettings> serviceApiSettings)
+        {
+            _httpClient = httpClient;
+            _httpContextAccessor = httpContextAccessor;
+            _clientSettings = clientSettings.Value;
+            _serviceApiSettings = serviceApiSettings.Value;
+        }
+
+        public async Task<Response<bool>> SignIn(SignInInput signInInput)
+        {
+            DiscoveryDocumentResponse discovery = await _httpClient.GetDiscoveryDocumentAsync(new DiscoveryDocumentRequest
+            {
+                Address = _serviceApiSettings.BaseUri,
+                Policy = new DiscoveryPolicy { RequireHttps = false }
+            });
+
+            if (discovery.IsError)
+            {
+                throw discovery.Exception;
+            }
+
+            PasswordTokenRequest passwordTokenRequest = new()
+            {
+                ClientId = _clientSettings.WebClientForUser.ClientId,
+                ClientSecret = _clientSettings.WebClientForUser.ClientSecret,
+                UserName = signInInput.Email,
+                Password = signInInput.Password,
+                Address = discovery.TokenEndpoint
+            };
+
+            TokenResponse token = await _httpClient.RequestPasswordTokenAsync(passwordTokenRequest);
+
+            if (token.IsError)
+            {
+                string responseContent = await token.HttpResponse.Content.ReadAsStringAsync();
+
+                ErrorDto errorDto = JsonSerializer.Deserialize<ErrorDto>(responseContent, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+
+                return Response<bool>.Fail(errorDto.Errors, 400);
+            }
+
+            UserInfoRequest userInfoRequest = new()
+            {
+                Token = token.AccessToken,
+                Address = discovery.UserInfoEndpoint
+            };
+
+            UserInfoResponse userInfo = await _httpClient.GetUserInfoAsync(userInfoRequest);
+
+            if (userInfo.IsError)
+            {
+                throw userInfo.Exception;
+            }
+
+            ClaimsIdentity claimsIdentity = new(userInfo.Claims, CookieAuthenticationDefaults.AuthenticationScheme, "name", "role");
+
+            ClaimsPrincipal claimsPrincipal = new(claimsIdentity);
+
+            AuthenticationProperties authenticationProperties = new();
+
+            authenticationProperties.StoreTokens(new List<AuthenticationToken>()
+            {
+                new AuthenticationToken
+                {
+                    Name = OpenIdConnectParameterNames.AccessToken,
+                    Value = token.AccessToken
+                },
+                new AuthenticationToken
+                {
+                    Name = OpenIdConnectParameterNames.RefreshToken,
+                    Value = token.RefreshToken
+                },
+                new AuthenticationToken
+                {
+                    Name = OpenIdConnectParameterNames.ExpiresIn,
+                    Value = DateTime.Now.AddSeconds(token.ExpiresIn).ToString("o", CultureInfo.InvariantCulture)
+                }
+            });
+
+            authenticationProperties.IsPersistent = signInInput.IsRemember;
+
+            await _httpContextAccessor.HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, claimsPrincipal, authenticationProperties);
+
+            return Response<bool>.Success(200);
+        }
+
+        public Task<TokenResponse> GetAccessTokenByRefreshToken()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task RevokeRefreshToken()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
