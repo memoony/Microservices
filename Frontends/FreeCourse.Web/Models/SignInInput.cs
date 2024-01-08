3749e271465beec82116ae566cab6623e20dd885
Mehmet Gürsoy <mehmet.gursoy@windowslive.com>
2024-01-08 23:27:19 +0300

    AuthController SignIn metodu, SignIn sayfası ve Program.cs cookie ayarları tamamlandı.

--- Frontends/FreeCourse.Web/Models/SignInInput.cs
+++ Frontends/FreeCourse.Web/Models/SignInInput.cs
@@ -4,12 +4,15 @@ namespace FreeCourse.Web.Models
 {
     public class SignInInput
     {
+        [Required]
         [Display(Name = "E-Posta Adresi")]
         public string Email { get; set; }
 
+        [Required]
         [Display(Name = "Şifre")]
         public string Password { get; set; }
 
+        [Required]
         [Display(Name = "Beni Hatırla")]
         public bool IsRemember { get; set; }
     }
--- Frontends/FreeCourse.Web/Program.cs
+++ Frontends/FreeCourse.Web/Program.cs
@@ -1,9 +1,23 @@
 using FreeCourse.Web.Models;
+using FreeCourse.Web.Services;
+using FreeCourse.Web.Services.Interfaces;
+using Microsoft.AspNetCore.Authentication.Cookies;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.Configure<ServiceApiSettings>(builder.Configuration.GetSection("ServiceApiSettings"));
 builder.Services.Configure<ClientSettings>(builder.Configuration.GetSection("ClientSettings"));
+
+builder.Services.AddHttpClient<IIdentityService, IdentityService>();
+builder.Services.AddHttpContextAccessor();
+
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme).AddCookie(CookieAuthenticationDefaults.AuthenticationScheme, options =>
+{
+    options.LoginPath = "/Auth/SignIn";
+    options.ExpireTimeSpan = TimeSpan.FromDays(60);
+    options.SlidingExpiration = true;
+    options.Cookie.Name = "webcookie";
+});
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
@@ -19,6 +33,7 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
