a869461e2546c9bf3a605ffaf9e27832dfe9dcfb
Mehmet Gürsoy <mehmet.gursoy@windowslive.com>
2024-03-19 15:31:00 +0300

    UserController ve ilgili Index sayfası eklendi.

--- Frontends/FreeCourse.Web/Program.cs
+++ Frontends/FreeCourse.Web/Program.cs
@@ -10,6 +10,8 @@ builder.Services.Configure<ServiceApiSettings>(builder.Configuration.GetSection(
 builder.Services.Configure<ClientSettings>(builder.Configuration.GetSection("ClientSettings"));
 builder.Services.AddHttpContextAccessor();
 
+builder.Services.AddScoped<ResourceOwnerPasswordTokenHandler>();
+
 var serviceApiSettings = builder.Configuration.GetSection("ServiceApiSettings").Get<ServiceApiSettings>();
 
 builder.Services.AddHttpClient<IIdentityService, IdentityService>();
