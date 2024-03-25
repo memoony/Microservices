97255c4f7e192a76306a27031a16c5fb832c9c64
Mehmet Gürsoy <mehmet.gursoy@windowslive.com>
2024-03-25 22:06:48 +0300

    CoursesController eklendi.

--- Frontends/FreeCourse.Web/Program.cs
+++ Frontends/FreeCourse.Web/Program.cs
@@ -1,3 +1,4 @@
+using FreeCourse.Shared.Services;
 using FreeCourse.Web.Handlers;
 using FreeCourse.Web.Models;
 using FreeCourse.Web.Services;
@@ -6,19 +7,22 @@ using Microsoft.AspNetCore.Authentication.Cookies;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Configures
 builder.Services.Configure<ServiceApiSettings>(builder.Configuration.GetSection("ServiceApiSettings"));
 builder.Services.Configure<ClientSettings>(builder.Configuration.GetSection("ClientSettings"));
+
 builder.Services.AddHttpContextAccessor();
 
+// DI Lifetimes
+builder.Services.AddScoped<ISharedIdentityService, SharedIdentityService>();
 builder.Services.AddScoped<ResourceOwnerPasswordTokenHandler>();
 
 var serviceApiSettings = builder.Configuration.GetSection("ServiceApiSettings").Get<ServiceApiSettings>();
 
+// AddHttpClients
 builder.Services.AddHttpClient<IIdentityService, IdentityService>();
-
 builder.Services.AddHttpClient<IUserService, UserService>(options => options.BaseAddress = new Uri(serviceApiSettings.IdentityBaseUri))
     .AddHttpMessageHandler<ResourceOwnerPasswordTokenHandler>();
-
 builder.Services.AddHttpClient<ICatalogService, CatalogService>(options => options.BaseAddress = new Uri($"{serviceApiSettings.GatewayBaseUri}/{serviceApiSettings.Catalog.Path}"));
 
 builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme).AddCookie(CookieAuthenticationDefaults.AuthenticationScheme, options =>
